c9dda0886885e50c707283ed4e9d7371d30e5f57
Vinzon <luongvinson@gmail.com>
2022-08-30 17:34:24 -0600

    complete. added dynamically updated rating on thank you message

--- 02_interactive_rating/script.js
+++ 02_interactive_rating/script.js
@@ -1,4 +1,23 @@
 let rateButtons = document.querySelectorAll('.rating');
+let rating;
 rateButtons.forEach(button => {
-    button.addEventListener('click', () => button.classList.toggle('active'))
+    button.addEventListener('click', () => {
+        resetButtons();
+        button.classList.toggle('active');
+        rating = button.textContent;        
+    })
 });
+
+function resetButtons() {
+    rateButtons.forEach(button => button.classList.remove('active'));
+}
+
+document.querySelector('.submit').addEventListener('click', () => {
+    document.querySelector('.welcome').classList.toggle('hide');
+    document.querySelector('.thankyou').classList.toggle('hide');
+    print(rating);
+})
+
+function print(rating) {
+    document.querySelector('.result').textContent = `You Selected ${rating} out of 5`;
+}
