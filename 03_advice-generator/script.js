39595fe4cb709eef3aa1636019241b97aa42c44c
Vinzon <luongvinson@gmail.com>
2022-08-31 17:29:13 -0600

    added functions to retrieve advice API, and to print advice

---
+++ 03_advice-generator/script.js
@@ -0,0 +1,16 @@
+function getAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((data) => printAdvice(data))
+        .catch((response => console.error(response.status)))
+}
+
+function printAdvice(advice) {
+    tagDiv = document.querySelector(".tag");
+    tagDiv.textContent = `ADVICE #${advice.slip.id}`
+
+    adviceDiv = document.querySelector(".advice");
+    adviceDiv.textContent = `" ${advice.slip.advice}"`
+}
+
+getAdvice();
