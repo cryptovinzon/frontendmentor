e991e434c502451e229f5feac13cc25fb57d376d
Vinzon <luongvinson@gmail.com>
2022-09-05 12:02:05 -0600

    added nav menu animations

--- 06_intro_with_nav/script.js
+++ 06_intro_with_nav/script.js
@@ -1,5 +1,6 @@
 document.querySelectorAll('.expand').forEach(expand => {
     expand.addEventListener('click', (e) => {
+        e.target.classList.toggle('active');
         e.target.nextElementSibling.classList.toggle('hidden')
     })
 })
